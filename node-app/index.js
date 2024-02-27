d9858f11bc86f0b55edea0c32541588e938de073
Charles Fabri <144963126+charlesrw1750@users.noreply.github.com>
2024-02-26 23:48:28 -0300

    Correção do Entrypoint

--- node-app/index.js
+++ node-app/index.js
@@ -1,19 +1,27 @@
-const express = require('express')
-const redis = require('redis')
+const express = require('express');
+const redis = require('redis');
 
-const app = express()
-const client = redis.createClient()
+const app = express();
+const client = redis.createClient({
+    host: 'redis-server',
+    port: 6379
+});
 
-client.set('visits',0);
+client.set('visits', 0);
 
-app.get('/', (req, res)=>{
-    client.get('visits', (err, visits)=>{
-        visits = parseInt(visits) + 1
-        res.send('Number of visits is:' + visits)
-        client.set('visits', parseInt(visits))
-    })
-})
+app.get('/', (req, res) => {
+    client.get('visits', (err, visits) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).send('Erro ao obter o número de visitas');
+        }
 
-app.listen(8081, ()=>{
-    console.log('Serviço na porta 8081')
-})
+        visits = parseInt(visits) + 1;
+        res.send('Número de visitas é: ' + visits);
+        client.set('visits', visits.toString());
+    });
+});
+
+app.listen(8081, () => {
+    console.log('Serviço na porta 8081');
+});
